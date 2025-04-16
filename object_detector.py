f4cf52618f72412c5b859ccbba245720256bb0a3
anurag.borse <anurag@gmail.com>
2025-04-16 12:25:48 +0200

    main.py

--- object_detector.py
+++ object_detector.py
@@ -13,48 +13,6 @@ class ObjectDetector:
         self.profile = self.pipeline.start(cfg)
         self.intrinsics = self.profile.get_stream(rs.stream.color).as_video_stream_profile().get_intrinsics()
         self.align = rs.align(rs.stream.color)
-
-<<<<<<< HEAD
-    def get_detection(self): # created function for getting object detection
-        frames = self.pipeline.wait_for_frames() # settung up the frames
-        aligned_frames = self.align.process(frames) # setting aligned frames
-        
-        color_frame = aligned_frames.get_color_frame() # setting the color frame
-        depth_frame = aligned_frames.get_depth_frame() # setting up the depth frame
- 
-        if not color_frame or not depth_frame: # if there is not color frame and depth frame 
-            return None # return None
-
-        color_image = np.asanyarray(color_frame.get_data())
-        gray = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
-        _, binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-
-        contours,_ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        best_contour = max(contours, key=cv2.contourArea, default=None)
-
-        if best_contour is None or cv2.contourArea(best_contour)<1000:
-            return None
-
-        rect = cv2.minAreaRect(best_contour) 
-        (center_x, center_y),_,angle=rect 
-        center_x,center_y = int(center_x),int(center_y)
-
-        depth_value = depth_frame.get_distance(center_x,center_y)
-        if depth_value<=0:
-            return None
-
-        xyz_camera = rs.rs2_deproject_pixel_to_point(self.intrinsics,[center_x,center_y],depth_value)
-        xyz_camera = np.array([*xyz_camera,1])
-
-        detection={
-            "pixel":(center_x,center_y),
-            "position_camera":xyz_camera[:3],
-            "orientation_deg":angle,
-            "color_image": color_image 
-        }
-        return detection
-=======
-        self.visualize = visualize
         self.latest_color_image = None
 
     def get_detection(self, retry_count=5, delay_sec=0.5, show_when_none=True):
